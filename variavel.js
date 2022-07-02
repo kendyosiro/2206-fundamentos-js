69cdf2e873d9bae97af5e9d8cabc96cbf360e906
Kendy Osiro <kendyosiro@gmail.com>
2022-07-02 11:38:09 -0300

    trabalhei com variavel.js

---
+++ variavel.js
@@ -0,0 +1,14 @@
+//var
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+
+let area;
+
+if (forma==='quadrado'){
+    area = altura * comprimento;
+}else{
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
